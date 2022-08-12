31bd0f75a6e10973185fd68736ed54021e36d760
pedro.cruz <pedro.cruz@imusica.com.br>
2022-08-11 23:26:26 -0300

    chore: add dashboard page

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,14 +1,24 @@
 import { NextPage } from "next";
-import { useContext } from "react";
+import { useRouter } from "next/router";
+import { useContext, useEffect } from "react";
 import { Header } from "../components/Header/Header";
 import { AuthContext } from '../providers/AuthContext';
 
 const Dashboard: NextPage = () => {
     const { user } = useContext(AuthContext);
+    const router = useRouter()
+
+    useEffect(() => {
+        if(user.logged === false) {
+            router.push("/login")
+        }
+    }, [user])
     return <div>
         <Header />
-        <h1>Seus depoimentos</h1>
-        <p>{user?.name}</p>
+
+        <main className="mt-10 text-lg">
+            <h1>Seus depoimentos</h1>
+        </main>
     </div>
 }
 
