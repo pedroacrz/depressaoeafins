c6442a80dad1fd85e2e7183998e9bb39c7427633
pedroacrz <ti.pedrocruz@gmail.com>
2022-08-17 15:36:05 -0300

    chore: add analytics config in app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,24 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { AuthProvider } from '../providers/AuthContext'
+import { useEffect } from 'react'
 import 'react-quill/dist/quill.snow.css'
+import * as gtag from '../lib/gtag'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const routerUse = useRouter();
+  
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    routerUse.events.on("routeChangeComplete", handleRouteChange)
+    return () => {
+      routerUse.events.off("routeChangeComplete", handleRouteChange)
+    };
+  }, [routerUse.events]);
+
   return <div>
     <AuthProvider>
       <Component {...pageProps} />
